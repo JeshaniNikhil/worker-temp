120faaf40612da00f2e5715a49a15f8c67b2a20e
JeshaniNikhil <webnikhil26@gmail.com>
2026-08-16 01:30:22 +0530

    Allow production host in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['wolf-crm.wolfgroupindia.com'],
     proxy: {
       '/api': 'http://localhost:8002'
     }
